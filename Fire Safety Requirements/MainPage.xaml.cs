cec4091a332741e2e83a87ab0a59333f6a7ebae6
alora_7uijwgi <alora_7uijwgi@FOXTROT11>
2024-11-12 20:05:44 +0800

    routing changes, assembly.html: added values ending with staged enclosed replace fsedlogo to new, disabled flyout menu replace text in mainpage

--- Fire Safety Requirements/MainPage.xaml.cs
+++ Fire Safety Requirements/MainPage.xaml.cs
@@ -8,10 +8,19 @@
         {
             InitializeComponent();
             programmer_message = "Good day, my fellow Fire Safety Inspectors (FSIs) and Building Plan Evaluators (BPEs)!\r\n\r\nI am Junrey B. Ongcol, the creator of this app. I developed this app to assist you, my fellow FSIs and BPEs, in performing our work with more guidance and ease.\r\n\r\nThis app is based entirely on our manual, Fire Safety Guidelines for Different Types of Occupancy, Vol. 2. I sincerely hope that this app proves to be a valuable tool in your work.";
-            preface = "Fire is extremely dangerous, and we must all be cautious of it to save our lives and the lives of our loved ones. Depending on the existing conditions, fire may start as a slow-growth scenario or it may grow rapidly in a building. In either case, fire is considered a disaster that, when not suppressed, can lead to a total loss of property and ultimately the loss of lives.\n\n Nowadays and in future generations, it is important to have a basic understanding of fire safety and prevention. The Bureau of Fire Protection is the sole agency mandated to enforce the Fire Code of the Philippines. As such, public safety is its highest obligation, and the prevention of destructive fires is among its utmost responsibilities.";
+            preface = "Welcome to the Fire Safety Guidelines App\r\n\r\n" +
+                "This app is based on Fire Safety Guidelines on Different Types of Occupancy, Volume 2, " +
+                "a comprehensive resource designed to help you understand and implement fire safety measures " +
+                "across various occupancy types. Whether you're a building manager, safety officer, " +
+                "or just interested in fire safety, this app provides essential guidelines to ensure the safety and security of occupants in diverse environments.\r\n\r\n" +
+                "Explore detailed recommendations and guidelines tailored to each occupancy type, and stay informed with the best practices for fire safety.";
             BindingContext = this;
         }
-
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Using Shell Navigation to go to the page
+            await Shell.Current.GoToAsync("//OccupancyTypePage");
+        }
     }
 
 }
